3c4819914977433d822ce23c1e1feb0bcb1faba2
Ilya <46574184+TitovIe@users.noreply.github.com>
2019-06-22 17:12:34 +0300

    Add files via upload

--- graph.h
+++ graph.h
@@ -11,30 +11,38 @@
 
 using namespace std;
 
-class Sample {
-public:
-    explicit Sample(mt19937& gen);
-    vector<int>& Get_sigma_vector();
-    void Print_sample();
-private:
-    vector<int> sigma_vector;
-    const pair<int, int> sigma_value = {-1, 1};
-};
+static int global_counter = 2;
 
 class Graph {
 public:
     explicit Graph(mt19937& gen);
-    vector<Sample>& Get_m_samples();
+    vector<vector<int>>& Get_m_samples();
     vector<vector<double>>& Get_Ji_vector();
+    vector<vector<double>>& Get_constraint_vector();
     void Gen_Ji_start_vector(mt19937& gen);
-    void Print_sigma();
+    void Gen_sigma_vector(mt19937& gen);
+
+    void Print_sigma_all();
+    void Print_sigma_sample(const vector<int>& sample);
     void Print_Ji();
 
-    static const int M = 5000, N = 25, beta = 1;
+    static const int M = 10000, N = 4, beta = 1;
     static constexpr double alfa = 0.4;
+    const double eps = 0.05;
+    const double c_lambda = 0.8;
+    double lambda =
+            c_lambda * sqrt(log(pow(N, 2) / eps) / M);
 private:
     vector<vector<double>> Ji_vector;
-    vector<Sample> M_samples;
+    vector<vector<double>> constraint_vector;
+    vector<vector<int>> sigmai_vector;
 };
 
+void MonteCarlo(vector<vector<int>>& samples, mt19937& gen);
+double Jij_sum_calc(const vector<vector<double>>& Jij_vector,
+        const vector<int>& sigma_vector);
+
 ostream& operator << (ostream& os, const vector<double>& v);
+
+Graph& GetClobalObject();
+void Jij_to_zero(Graph& g);
--- main.cpp
+++ main.cpp
@@ -1,29 +1,54 @@
+#include <ifopt/problem.h>
+#include <ifopt/ipopt_solver.h>
 #include "graph.h"
-#include "gradient_method.h"
+#include "problem_opt2.h"
+//#include </home/titov/CoinIpopt/include/coin/IpIpoptApplication.hpp>
+
+//using namespace Ipopt;
+
+using namespace ifopt;
+
 int main() {
-    /*
-    ifstream input_bonds(R"(C:\Users\ILYA\CLionProjects\Diplom\alanin.psf)"),
-             input_q(R"(C:\Users\ILYA\CLionProjects\Diplom\alanin.psf)"),
-             input_xyz(R"(C:\Users\ILYA\CLionProjects\Diplom\alanin.txt)");
+    //mt19937 gen;
+    //gen.seed(time(0));
 
-    Atom atom;
-
-    atom.Create_bonds(input_bonds);
-    //PrintBonds(atom);
-    atom.Create_q(input_q);
-    //PrintQ(atom);
-    atom.Create_xyz(input_xyz);
-    PrintAll(atom);
-    */
-
-    mt19937 gen;
-    gen.seed(time(0));
-
-    Graph graph(gen);
+    Graph graph = GetClobalObject();
+    graph.Print_sigma_all();
     graph.Print_Ji();
 
-    Graph_reconst_calc(graph, 0.00005);
-    graph.Print_Ji();
+    //Graph_reconst_calc(graph, 0.001);
+    //graph.Print_Ji();
+    
+    //for(size_t i = 0; i < 2; i++) {
+       // for (; global_counter < Graph::N; global_counter++) {
+            Problem nlp;
+            nlp.AddVariableSet(make_shared<ExVariables>());
+            nlp.AddConstraintSet(make_shared<ExConstraint>());
+            nlp.AddCostSet(make_shared<ExCost>());
+            nlp.PrintCurrent();
+
+            // 2. choose solver and options
+            IpoptSolver ipopt;
+            ipopt.SetOption("linear_solver", "mumps");
+            ipopt.SetOption("jacobian_approximation", "exact");
+
+            // 3 . solve
+            ipopt.Solve(nlp);
+            Eigen::VectorXd x = nlp.GetOptVariables()->GetValues();
+            cout << x.transpose() << endl;
+        //}
+        //Jij_to_zero(graph);
+        //graph.lambda = 0;
+        //global_counter = 0;
+    //}
+
+    //graph.Print_Ji();
+    // 4. test if solution correct
+    //double eps = 1e-5; //double precision
+    //assert(1.0-eps < x(0) && x(0) < 1.0+eps);
+    //assert(0.0-eps < x(1) && x(1) < 0.0+eps);
+    
+    
     //graph.Print_graph();
     //cout << S_calculate(graph);
     //cout << Gradient();
@@ -31,152 +56,3 @@ int main() {
     return 0;
 }
 
-/*
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <map>
-#include <set>
-#include <tuple>
-#include <vector>
-
-//#include <random>
-//#include <ctime>
-//#include <ip>
-
-using namespace std;
-
-using Type_atom = map<int, tuple<set<int>, double, vector<double>>>;
-
-ostream& operator<<(ostream& os, const set<int>& s) {
-    for(const auto& i : s)
-        cout << i << " ";
-
-    return os;
-}
-
-ostream& operator<<(ostream& os, const vector<double>& v) {
-    for(const auto& i : v)
-        cout << i << " ";
-
-    return os;
-}
-
-ostream& operator<<(ostream& os, const tuple<set<int>, double, vector<double>>& t) {
-    cout << get<0>(t) << " ";
-    cout << get<1>(t) << " ";
-    cout << get<2>(t);
-
-    return os;
-}
-
-ostream& operator<<(ostream& os, const Type_atom& t) {
-    for(const auto& item : t) {
-        cout << item.first << ": " << item.second << endl;
-    }
-    return os;
-}
-
-class Atom {
-private:
-    Type_atom atoms;
-public:
-    void Create_bonds(ifstream& input) {
-        string line;
-        int atom1, atom2;
-
-        while (getline(input, line)) {
-            if(line.find("bonds") != string::npos) {
-                break;
-            }
-        }
-
-        getline(input, line);
-        while(line.find("angles") == string::npos) {
-            stringstream ss(line);
-
-            while (ss) {
-                ss >> atom1;
-
-                if(ss.eof())
-                    break;
-
-                ss >> atom2;
-
-                get<0>(atoms[atom1]).insert(atom2);
-                get<0>(atoms[atom2]).insert(atom1);
-            }
-
-            getline(input, line);
-        }
-    }
-    void Create_q(ifstream& input) {
-        string line;
-        string q;
-
-        while (getline(input, line)) {
-            if(line.find("NATOM") != string::npos) {
-                break;
-            }
-        }
-
-        getline(input, line);
-        for (auto &atom : atoms) {
-            stringstream ss(line);
-
-            for(int i = 0; i < 8; i++) {
-                ss >> q;
-            }
-
-            get<1>(atom.second) = atof(q.c_str());
-            getline(input, line);
-        }
-    }
-    void Create_xyz(ifstream& input) {
-        string line, x, y, z;
-
-        getline(input, line);
-        while (getline(input, line)) {
-            if(line.find("created by user:") != string::npos) {
-                break;
-            }
-        }
-
-        getline(input, line);
-        for (auto &atom : atoms) {
-            stringstream ss(line);
-
-            for(int i = 0; i < 8; i++) {
-                if(i < 6)
-                    ss >> x;
-                else if(i == 6)
-                    ss >> y;
-                else if(i == 7 )
-                    ss >> z;
-            }
-
-            get<2>(atom.second).push_back(atof(x.c_str()));
-            get<2>(atom.second).push_back(atof(y.c_str()));
-            get<2>(atom.second).push_back(atof(z.c_str()));
-            getline(input, line);
-        }
-    }
-    Type_atom GetAtoms() const {
-        return atoms;
-    }
-};
-
-void PrintBonds(const Atom& atom) {
-    for(const auto& b : atom.GetAtoms())
-        cout << b.first << ": " << get<0>(b.second) << endl;
-}
-
-void PrintQ(const Atom& atom) {
-    for(const auto& b : atom.GetAtoms())
-        cout << b.first << ": " << get<1>(b.second) << endl;
-}
-
-void PrintAll(const Atom& atom) {
-    cout << atom.GetAtoms();
-}
-*/
